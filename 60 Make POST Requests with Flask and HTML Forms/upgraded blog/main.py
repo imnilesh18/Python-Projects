a95b16b17658cdf3870e6fd5a6bb83acc66ef8f4
Nilesh <nileshshrivastava7737@gmail.com>
2023-05-27 22:20:48 +0530

    day 60

---
+++ 60 Make POST Requests with Flask and HTML Forms/upgraded blog/main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+
+posts = requests.get("https://api.npoint.io/53f5792c63825098b7ad").json()
+OWN_EMAIL = "YOUR EMAIL"
+OWN_PASSWORD = "YOUR APP GENERATED PASSWORD"
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
